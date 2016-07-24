5c685f9f7e6f70e88e596860c0cfa9b3c63f9848
Melanie <email": null>
2016-07-24 12:15:35 -0700

    Create skeleton controller actions for users and sessions

--- app/controllers/actors-controller.rb
+++ app/controllers/actors-controller.rb
@@ -1,19 +1,28 @@
-# Sign up
-
-
-# Log in
-
-
-
-# Log out
-
-
-
 get '/actors' do
   "This is an actor's home page"
+end
+
+
+# Sign up
+get '/actors/new' do
 end
 
 post '/actors' do
   # create a new actor and save to the database
   redirect '/actors'
 end
+
+# Log in
+get '/sessions/new' do
+end
+
+post '/sessions' do
+end
+
+# Log out
+delete '/sessions/:id' do
+end
+
+
+
+
