c068d9df325423f6cb03f4e4fb7ee3a4cd22f2bc
Melanie <email": null>
2016-07-24 18:39:39 -0700

    Create part of add new post form

--- app/controllers/users-controller.rb
+++ app/controllers/users-controller.rb
@@ -8,7 +8,7 @@ post '/users' do
   # create a new actor and save to the database
   user = User.create(params[:user])
   if user.valid?
-    redirect '/users'
+    redirect "/users/#{user.id}"
   else
     status 422
     @errors = user.errors.full_messages
