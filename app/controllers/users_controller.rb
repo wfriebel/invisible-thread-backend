b4cbddfc9290cfd1ca44075bd1a16cc1c71a67b3
Calvin Kwong <ckwong93@gmail.com>
2016-08-17 16:49:51 -0700

    testing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 get '/users' do
+	@user = User.create(name: "Tom", password: "123", longitude: 123.456789, latitude: -123.456789)
 	@users = User.where(name: params[:name], password: params[:password])
 	erb :users
 end
